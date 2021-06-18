03eab612adfdb49832069a5a858f28c9a4478d1f
CR-MX <31296448+CR-MX@users.noreply.github.com>
2021-06-17 23:27:36 -0500

    base

---
+++ encuestas/usuarios/urls.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+from . import templates
+
+app_name='eduacionapp'
+
+#Aqui se determinan las urls a las que se puede acceder a la página
+urlpatterns = [
+    path('home/', views.Home.as_view(), name='home'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('register/', views.Register.as_view(), name='register'),
+    path(r'^logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
---
+++ encuestas/usuarios/views.py
@@ -0,0 +1,41 @@
+from .forms import UsuarioForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from .decorators import unauthenticated_user
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
+from django.shortcuts import render
+
+
+#Esta clase se utiliza para realizar el registro de un nuevo usuario en la plataforma web
+@method_decorator(unauthenticated_user, name='dispatch')
+class Register(CreateView):
+    model = User
+    template_name = "register.html"
+    form_class = UsuarioForm
+    success_url = reverse_lazy('eduacionapp:login')
+
+#Esta clase se utiliza para iniciar sesion dentro de la plataforma web
+@method_decorator(unauthenticated_user, name='dispatch')
+class Login(LoginView):
+    template_name = 'login.html'
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('eduacionapp:admin')
+
+#Esta clase es para organizar el contenido base de la plataforma web
+class Home (TemplateView):
+    template_name = 'Home.html'
+    success_url = reverse_lazy('eduacionapp:admin')
+    usuarios = User.objects.all()
+    extra_context = ({'usuarios': usuarios})
+
+    def lista(request):
+        usuarios = User.objects.all()
+        return render(request, 'home.html', {'usuarios': usuarios})
+
+class ListaUsuario(ListView):
+    model = User
